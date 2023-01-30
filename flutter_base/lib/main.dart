b1adea7325cb8d972c69c831b7f968450e1c7790
zhfei <1174977961@qq.com>
2023-01-31 00:08:34 +0800

    feat：商品页添加商品列表

--- flutter_base/lib/main.dart
+++ flutter_base/lib/main.dart
@@ -14,14 +14,44 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('商品列表'),
       ),
-      body: Text('Hello Flutter'),
+      body: MyContentBody(),
+    );
+  }
+}
+
+class MyContentBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+        children: [
+          MyShoppingItem('iPhone 13', "大屏 好看", 'https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/4023ed80e2794fb48858b4809b17f139~tplv-k3u1fbpfcp-no-mark:480:400:0:0.image?'),
+          MyShoppingItem('iPhone 13', "大屏 好看", 'https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/4023ed80e2794fb48858b4809b17f139~tplv-k3u1fbpfcp-no-mark:480:400:0:0.image?'),
+          MyShoppingItem('iPhone 13', "大屏 好看", 'https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/4023ed80e2794fb48858b4809b17f139~tplv-k3u1fbpfcp-no-mark:480:400:0:0.image?'),
+        ],
+    );
+  }
+}
+
+class MyShoppingItem extends StatelessWidget {
+  final title;
+  final desc;
+  final imageUrl;
+  MyShoppingItem(this.title, this.desc, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(this.title),
+        Text(this.desc),
+        Image.network(this.imageUrl)
+      ],
     );
   }
 }
